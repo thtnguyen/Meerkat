9598981dd2a3a8944f5b0c25ce029cd78410fb85
quandoan97 <qdoan97@gmail.com>
2020-02-05 15:52:58 -0600

    added fields on the registration page

--- meerkat-frontend/src/components/pages/Register.js
+++ meerkat-frontend/src/components/pages/Register.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import { TextField, Button } from '@material-ui/core/'
 
 export default class Register extends Component {
+    handleSubmit(event){
+        alert();
+    }
     render() {
         return (
             <div>
                 <h1> Register Page </h1>
+                <form onSubmit={ this.handleSubmit }>
+                    <label> First Name </label>
+                    <TextField id="fName" ></TextField>
+                    <Button variant="contained" onClick={this.handleSubmit}> Submit </Button>
+                </form>
             </div>
         )
     }
