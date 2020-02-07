fff59123a8d1a466a1c2382aa0be5f07577d9fdf
quandoan97 <qdoan97@gmail.com>
2020-02-06 20:30:09 -0600

    more validation on the registration page

--- meerkat-frontend/src/components/pages/Register.js
+++ meerkat-frontend/src/components/pages/Register.js
@@ -13,28 +13,37 @@ export default function Register(){
         return (
             <div>
                 <h1> Register Page </h1>
+
                 <form onSubmit={ handleSubmit(onSubmit) }>
                     <div className="container">
                         <div className="row"> 
                             <div className="col"> 
-                                <input type="text" name="fName" placeholder="First Name" ref={ register }/> 
+                                <input type="text" name="fName" placeholder="First Name" ref={ register({ required: "Please enter first name", minLength: {value: 2, message: "Name must be at least 2 characters"} }) }/> 
+                                {errors.fName && <p> { errors.fName.message } </p>}
                             </div>
                             <div className="col">  
-                                <input type="text" name="lName" placeholder="Last Name" ref={ register }/> 
+                                <input type="text" name="lName" placeholder="Last Name" ref={ register({ required: true, minLength: {value: 2, message: "Name must be at least 2 characters"} }) }/> 
+                                {errors.lName && <p> { errors.lName.message } </p>}
                             </div>
                             <div className="col">  
                                 <input type="text" name="uName" placeholder="Username" ref={ register }/> 
                             </div>
                             <div className="col">  
-                                <input type="text" name="password" placeholder="Password" ref={ register({required: "Please Enter password", minLength: {value: 8, message:"Password too short" }}) }/> 
+                                <input type="password" name="password" placeholder="Password" ref={ register({ required: "Please Enter password", minLength: {value: 8, message:"Password too short" }}) }/> 
+                                {errors.password && <p> {errors.password.message} </p>}
+                            </div>
+                            <div className="row">
+                                <div className="col">
+                                    <input type="email" name="emailAddress" placeholder="Email Address"/>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    {errors.password && alert(errors.password.message)}
                     <div>
                         <Button variant="contained" onClick={ handleSubmit(onSubmit) }> Submit </Button>
                     </div>
                 </form>
+
             </div>
         );
 }
