c150eec5592bc91f81175bde0e54fe1a2a6507d7
quandoan97 <qdoan97@gmail.com>
2020-02-13 20:07:09 -0600

    Added a logo and link to the homepage when clicking said logo

--- meerkat-frontend/src/components/NavigationBar.js
+++ meerkat-frontend/src/components/NavigationBar.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class NavigationBar extends Component {
     render() {
         return (
-        <Navbar bg="light" expand="lg">
-            <h3>Logo</h3>
-            <ul id="NavBar">
-                <li>
-                    <Link to='/'> Home </Link>
-                </li>
-            </ul>
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand > 
+                <Link to="/"> Meerkat </Link>
+            </Navbar.Brand>
         </Navbar>
         )
     }
--- meerkat-frontend/src/components/pages/UserPage.js
+++ meerkat-frontend/src/components/pages/UserPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function UserPage() {
     return (
         <div>
-            <h1>Login Page</h1>
+            <h1>User Page</h1>
         </div>
     );
 }
