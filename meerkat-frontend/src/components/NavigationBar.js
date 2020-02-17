caf7bcdbaaf23cd7ecfc7ba7b95b3de3c39b9b35
unknown <qdoan97@gmail.com>
2020-02-17 15:59:31 -0600

    Fixed the navigation bar to now have correct color

--- meerkat-frontend/src/components/NavigationBar.js
+++ meerkat-frontend/src/components/NavigationBar.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css'
+
 
 export default class NavigationBar extends Component {
     render() {
         return (
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand > 
-                <Link to="/"> Meerkat </Link>
-            </Navbar.Brand>
-        </Navbar>
+        <div id="navbar-div-background">
+            <nav class="navbar navbar-static-top">
+                <Navbar.Brand > 
+                    <Link to="/"><img src="../../images/standin-logo"></img></Link>
+                </Navbar.Brand>
+            </nav>
+        </div>
+        
         )
     }
 }
