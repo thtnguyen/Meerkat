73b0690e5d75ef2cd1d82413c92e091b0e0c3835
quandoan97 <qdoan97@gmail.com>
2020-02-25 17:10:48 -0600

    Working on data from the register page to an endpoint

--- meerkat-frontend/src/components/pages/Register.js
+++ meerkat-frontend/src/components/pages/Register.js
@@ -7,11 +7,24 @@ export default function Register(){
     const {register, handleSubmit, errors} = useForm();
         
     const onSubmit = (data) => {
-        fetch('https://webhook.site/67b9626f-2c2c-4cfc-8b98-d4813d414d42', {
-            method: 'POST',
-            body: JSON.stringify(data)
-        })
+            fetch('https://webhook.site/67b9626f-2c2c-4cfc-8b98-d4813d414d42', {
+                method: 'POST',
+                mode: 'no-cors',
+                headers:{
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', data)
+            })
+            .catch((error) => {
+                console.error('Error', error);
+            })
     };
+
 
         return (
             <div>
@@ -28,13 +41,13 @@ export default function Register(){
                                 <input type="text" name="lName" placeholder="Last Name" ref={ register({ required: true, minLength: {value: 2, message: "Name must be at least 2 characters"} }) }/> 
                                 {errors.lName && <p> { errors.lName.message } </p>}
                             </div>
-                            <div className="col">  
+                            {/* <div className="col">  
                                 <input type="text" name="uName" placeholder="Username" ref={ register }/> 
                             </div>
                             <div className="col">  
                                 <input type="password" name="password" placeholder="Password" ref={ register({ required: "Please Enter password", minLength: {value: 8, message:"Password too short" }}) }/> 
                                 {errors.password && <p> {errors.password.message} </p>}
-                            </div>
+                            </div> */}
                             <div className="row">
                                 <div className="col">
                                     <input type="email" name="emailAddress" placeholder="Email Address"/>
@@ -43,7 +56,7 @@ export default function Register(){
                         </div>
                     </div>
                     <div>
-                        <Button variant="contained" onClick={ handleSubmit(onSubmit) }> Submit </Button>
+                        <Button variant="contained" onClick={ handleSubmit(onSubmit()) }> Submit </Button>
                     </div>
                 </form>
 
