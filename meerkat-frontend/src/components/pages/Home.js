48454193127c5395a8ed4c2b8781e672bf90e06f
sauravsubedi25 <49078146+sauravsubedi25@users.noreply.github.com>
2020-03-04 18:00:21 -0600

    moving Profile and Create room to side nav test

--- meerkat-frontend/src/components/pages/Home.js
+++ meerkat-frontend/src/components/pages/Home.js
@@ -9,10 +9,10 @@ export default class Home extends Component {
         return (
             <div>
                 <div id="mySidenav" class="sidenav">
-                    <a href="/create" id="create">Create Rooms</a>
+                    <a href="/userpage" id="profile">Profile</a>
+                    <a href="/create" id="create">Create Room</a>
                     <a href="/View" id="view">View Rooms</a>
                     <a href="#" id="about">About Us</a>
-                    <a href="#" id="nothing">XYzhere</a>
                 </div>
                 <footer class="container-fluid text-center">
                     <p>All rights reserved || Meerkat</p>
