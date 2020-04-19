fa068690f26c5eaee1741402ccced6dd988aff5a
Quan Doan <qdoan97@gmail.com>
2020-04-18 22:27:05 -0500

    Added red error text and updated the UI for register page

--- meerkat-frontend/src/components/pages/Register.js
+++ meerkat-frontend/src/components/pages/Register.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextField, Button,Input } from '@material-ui/core/';
+import { TextField, Button, Input, Chip } from '@material-ui/core/';
 import { useForm } from 'react-hook-form';
 import { useHistory } from 'react-router-dom';
 
@@ -7,11 +7,23 @@ export default function Register(){
     //variable used to redirect users after registration 
     const history = useHistory();
 
-    const {register, handleSubmit, errors} = useForm();
+    const {register, handleSubmit, errors} = useForm({
+        defaultValues: {
+
+        }
+    });
+
+    const [chipData, setChipData] = React.useState([
+        { key: 0, label: 'Angular' },
+        { key: 1, label: 'jQuery' },
+        { key: 2, label: 'Polymer' },
+        { key: 3, label: 'React' },
+        { key: 4, label: 'Vue.js' },
+    ]);
     
     const onSubmit = (data, error, props) => {
         //The first url is used to bypass CORS error
-        fetch('https://cors-anywhere.herokuapp.com/'+'https://webhook.site/42fb35aa-a83e-4ee2-be2a-6a20ba2dfc7a', {
+        fetch('https://cors-anywhere.herokuapp.com/'+'https://webhook.site/0fa7de81-e70b-4085-9716-b3e11c982f5f', {
             method: 'POST',
             headers:{
                 'Accept': 'application/json',
@@ -28,33 +40,84 @@ export default function Register(){
             console.error('Error', error);
         })
     };
-
     
         return (
             <div>
                 <h1> Register Page </h1>
 
-                <form onSubmit={ handleSubmit(onSubmit) }>
+                <form onSubmit={ handleSubmit(handleSubmit(data => console.log(data))) }>
                     <div className="container">
                         <div className="row"> 
                             <div className="col"> 
-                                <input type="text" name="fName" placeholder="First Name" ref={ register({ required: "Please enter first name", minLength: {value: 2, message: "Name must be at least 2 characters"} }) }/> 
-                                {errors.fName && <p> { errors.fName.message } </p>}
+                                <TextField 
+                                    type="text" 
+                                    name="fName" 
+                                    placeholder="First Name" 
+                                    inputRef={ 
+                                        register({ 
+                                            required: "Please enter first name", 
+                                            minLength: {value: 2, message: "Name must be at least 2 characters"} 
+                                            }) 
+                                        }
+                                /> 
+                                {errors.fName && <p className="p-error"> { errors.fName.message } </p>}
                             </div>
+
                             <div className="col">  
-                                <input type="text" name="lName" placeholder="Last Name" ref={ register({ required: true, minLength: {value: 2, message: "Name must be at least 2 characters"} }) }/> 
-                                {errors.lName && <p> { errors.lName.message } </p>}
+                                <Input 
+                                    type="text" 
+                                    name="lName" 
+                                    placeholder="Last Name" 
+                                    inputRef={ 
+                                        register({ 
+                                            required: "Please enter last name", 
+                                            minLength: {value: 2, message: "Name must be at least 2 characters"} }) 
+                                        }
+                                    /> 
+                                {errors.lName && <p className="p-error"> { errors.lName.message } </p> }
                             </div>
+
                             <div className="col">  
-                                <input type="text" name="uName" placeholder="Username" ref={ register }/> 
+                                <Input 
+                                    type="text" 
+                                    name="uName" 
+                                    placeholder="Username" 
+                                    inputRef={ 
+                                        register({ 
+                                            required: "Please enter Username", 
+                                            minLength: {value: 2, message: "Name must be at least 2 characters"} }) 
+                                        }
+                                    /> 
+                                {errors.lName && <p className="p-error"> { errors.lName.message } </p>}
                             </div>
+
                             <div className="col">  
-                                <input type="password" name="password" placeholder="Password" ref={ register({ required: "Please Enter password", minLength: {value: 8, message:"Password too short" }}) }/> 
-                                {errors.password && <p> { errors.password.message } </p>}
+                                <Input 
+                                    type="password" 
+                                    name="password" 
+                                    placeholder="Password" 
+                                    inputRef={ 
+                                        register({ 
+                                            required: "Please Enter password", 
+                                            minLength: {value: 8, message:"Password too short" }}) 
+                                        }
+                                    /> 
+                                {errors.password && <p className="p-error"> { errors.password.message } </p>}
                             </div>
+
                             <div className="col">
-                                <input type="email" name="emailAddress" placeholder="Email Address" ref={ register({required: 'Required',pattern: {value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, message: "invalid email address"}}) }/>
-                                {errors.emailAddress && <p> { errors.emailAddress.message } </p>}
+                                <Input 
+                                    type="email" 
+                                    name="emailAddress" 
+                                    placeholder="Email Address" 
+                                    inputRef={ 
+                                        register({
+                                            required: 'Required',
+                                            pattern: {value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, 
+                                            message: "invalid email address"}}) 
+                                        }
+                                    />
+                                {errors.emailAddress && <p className="p-error"> { errors.emailAddress.message } </p>}
                             </div>
                         </div>
                     </div>
